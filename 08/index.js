d27afbfef75ab66d4efddc20413b93ac487a727f
James Rishe <jlrishe@users.noreply.github.com>
2017-12-10 04:53:43 +0900

    Day 8

---
+++ 08/index.js
@@ -0,0 +1,57 @@
+const { __, compose, map, filter, add, subtract, lt, gt, lte, gte, equals, complement, reduce, when, curry, isNil, always, apply, values, prop, merge, max } = require('ramda');
+const { probe } = require('../shared');
+
+const instrucPattern = /([a-z]+) (inc|dec) (-?\d+) if ([a-z]+) ([<>=!]+) (-?\d+)/;
+
+const applyPattern = str => instrucPattern.exec(str);
+
+const ops = { inc: add, dec: subtract };
+
+const comps = {
+    '<': lt,
+    '>': gt,
+    '<=': lte,
+    '>=': gte,
+    '==': equals,
+    '!=': complement(equals)
+};
+
+const instrucPartsToObj = ([_, reg, op, amt, cReg, comp, val]) => ({ 
+    reg, 
+    op: ops[op], 
+    amt: Number(amt), 
+    cReg, 
+    comp: comps[comp], 
+    val: Number(val) 
+});
+
+const parseInstruc = compose(instrucPartsToObj, applyPattern);
+
+const regValue = name => compose(when(isNil, always(0)), prop(name));
+
+const shouldApply = ({ cReg, comp, val }) => compose(comp(__, val), regValue(cReg));
+
+const applyInstruc = curry(({ reg, op, amt }, regs) => 
+    merge(regs, { [reg]: op(regValue(reg)(regs), amt)})
+);
+
+const checkAndApplyInstruc = curry((regs, instruc) => when(shouldApply(instruc), applyInstruc(instruc))(regs));
+
+const largestReg = compose(apply(Math.max), values);
+
+const p1 = compose(largestReg, reduce(checkAndApplyInstruc, {}), map(parseInstruc));
+
+const applyInstrucKeepMax = ({ regs, maxReg }, instruc) => {
+    const nextRegs = checkAndApplyInstruc(regs, instruc);
+    
+    return { regs: nextRegs, maxReg: max(maxReg, largestReg(nextRegs)) };
+};
+
+const p2 = compose(prop('maxReg'), reduce(applyInstrucKeepMax, { regs: {}, maxReg: 0 }), map(parseInstruc));
+
+module.exports = {
+    parseInstruc,
+    shouldApply,
+    applyInstruc,
+    ps: [p1, p2]
+};
---
+++ test/08.js
@@ -0,0 +1,53 @@
+const day = '08';
+
+const assert = require('assert');
+const { add, subtract, gt, lt, gte, equals } = require('ramda');
+const { ps, parseInstruc, shouldApply, applyInstruc } = require(`../${day}`);
+const [p1, p2] = ps;
+
+describe(`day ${day}`, () => {
+    const instrucs = [
+        'b inc 5 if a > 1',
+        'a inc 1 if b < 5',
+        'c dec -10 if a >= 1',
+        'c inc -20 if c == 10'
+    ];
+
+    
+    
+    
+    const expectedParsed = [
+        { reg: 'b', op: add,      amt: 5,   cReg: 'a', comp: gt,     val: 1  },
+        { reg: 'a', op: add,      amt: 1,   cReg: 'b', comp: lt,     val: 5  },    
+        { reg: 'c', op: subtract, amt: -10, cReg: 'a', comp: gte,    val: 1  },    
+        { reg: 'c', op: add,      amt: -20, cReg: 'c', comp: equals, val: 10 }    
+    ]
+    
+    it('should parse instructions', () => {
+        assert.deepEqual(parseInstruc(instrucs[0]), expectedParsed[0]);
+        assert.deepEqual(parseInstruc(instrucs[1]), expectedParsed[1]);
+        assert.deepEqual(parseInstruc(instrucs[2]), expectedParsed[2]);
+        assert.deepEqual(parseInstruc(instrucs[3]), expectedParsed[3]);
+    });
+    
+    it('should decide when to apply instructions', () => {
+        assert.ok(shouldApply(expectedParsed[0])({ a: 2 }), 'a 2');
+        assert.equal(shouldApply(expectedParsed[0])({ a: 1 }), false, 'a 1');
+        assert.ok(shouldApply(expectedParsed[1])({  }), 'empty');
+    });
+    
+    it('should apply instructions', () => {
+        assert.deepEqual(applyInstruc(expectedParsed[0])({ }), { b: 5 });
+        assert.deepEqual(applyInstruc(expectedParsed[0])({ b: 2 }), { b: 7 });
+        assert.deepEqual(applyInstruc(expectedParsed[2])({ }), { c: 10 });
+        assert.deepEqual(applyInstruc(expectedParsed[2])({ c: 3, a: 4 }), { c: 13, a: 4 });
+    });
+    
+    it('should work on samples for p1', () => {
+        assert.equal(p1(instrucs), 1);
+    });
+    
+    it('should work on samples for p2', () => {
+        assert.equal(p2(instrucs), 10);
+    });
+});

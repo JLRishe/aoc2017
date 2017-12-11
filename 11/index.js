37d52af4b23f85014b059ad21e92f50ddf6791de
James Rishe <jlrishe@users.noreply.github.com>
2017-12-11 16:28:26 +0900

    Day 11

---
+++ 11/index.js
@@ -0,0 +1,54 @@
+const { __, compose, map, filter, curry, split, reduce, prop, max, merge } = require('ramda');
+const { probe, repeatUntil, add1 } = require('../shared');
+
+const stepMap = {
+    n:  { x:  0, y:  2 },
+    ne: { x:  1, y:  1 },
+    se: { x:  1, y: -1 },
+    s:  { x:  0, y: -2 },
+    sw: { x: -1, y: -1 },
+    nw: { x: -1, y:  1 }
+};
+
+const takeStep = ({ cur: { x: posx, y: posy }, maxDist }, { x: sx, y: sy}) => {
+    const nextStep = { x: sx + posx, y: sy + posy };
+    
+    return { cur: nextStep, maxDist: max(maxDist, findDist(nextStep)) };
+};
+
+const walkSteps = reduce(takeStep, { cur: { x: 0, y: 0 }, maxDist: 0 });
+
+const moveCoordTowardOrigin = c => c - c / Math.abs(c);
+
+const stepTowardOrigin = ({ x, y, steps }) => 
+    ({ x: moveCoordTowardOrigin(x), y: moveCoordTowardOrigin(y), steps: add1(steps) });
+
+const findDist = compose(
+    ({x, y, steps }) => steps + Math.abs(y) / 2 + Math.abs(x),
+    repeatUntil(
+        stepTowardOrigin,
+        ({ x, y }) => x === 0 || y === 0
+    ),
+    merge(__, { steps: 0 })
+);
+
+const walkInput = compose(
+    walkSteps,
+    map(prop(__, stepMap)),
+    split(',')
+);
+
+const p1 = compose(
+    findDist,
+    prop('cur'),
+    walkInput
+);
+
+const p2 = compose(
+    prop('maxDist'),
+    walkInput
+);
+
+module.exports = {
+    ps: [p1, p2]
+};
---
+++ test/11.js
@@ -0,0 +1,23 @@
+const day = '11';
+
+const assert = require('assert');
+const { ps } = require(`../${day}`);
+const [p1, p2] = ps;
+
+describe(`day ${day}`, () => {
+    it('should work on samples for p1', () => {
+        assert.equal(p1('ne,ne,ne'), 3);
+        assert.equal(p1('ne,ne,sw,sw'), 0);
+        assert.equal(p1('ne,ne,s,s'), 2);
+        assert.equal(p1('se,sw,se,sw,sw'), 3);
+        assert.equal(p1('ne,se,ne,se,ne,s'), 5);
+    });
+    
+    it('should work on samples for p2', () => {
+        assert.equal(p2('ne,ne,ne'), 3);
+        assert.equal(p2('ne,ne,sw,sw'), 2);
+        assert.equal(p2('ne,ne,s,s'), 2);
+        assert.equal(p2('se,sw,se,sw,sw'), 3);
+        assert.equal(p2('ne,se,ne,se,ne,s,nw'), 5);
+    });
+});

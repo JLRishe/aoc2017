03c2bae9bb8ed8094d467b185d3eefd4d456a6db
James Rishe <jlrishe@users.noreply.github.com>
2017-12-12 19:42:40 +0900

    Refactor Day 11

--- 11/index.js
+++ 11/index.js
@@ -1,53 +1,65 @@
-const { __, compose, map, filter, curry, split, reduce, prop, max, merge } = require('ramda');
+const { __, compose, map, filter, curry, split, reduce, prop, min, max, merge, subtract, memoizeWith, identity } = require('ramda');
 const { probe, repeatUntil, add1 } = require('../shared');
 
+// Number -> Number -> CartPair
+const cartPair = (x, y) => ({ x, y });
+
 const stepMap = {
-    n:  { x:  0, y:  2 },
-    ne: { x:  1, y:  1 },
-    se: { x:  1, y: -1 },
-    s:  { x:  0, y: -2 },
-    sw: { x: -1, y: -1 },
-    nw: { x: -1, y:  1 }
+    n:  cartPair( 0,  2),
+    ne: cartPair( 1,  1),
+    se: cartPair( 1, -1),
+    s:  cartPair( 0, -2),
+    sw: cartPair(-1, -1),
+    nw: cartPair(-1,  1)
 };
 
-const takeStep = ({ cur: { x: posx, y: posy }, maxDist }, { x: sx, y: sy}) => {
-    const nextStep = { x: sx + posx, y: sy + posy };
+// CartPair -> Number
+const findDist = (pt) => {
+    const absPt = map(Math.abs, pt);
+    const diagDist = min(absPt.x, absPt.y);
+    const afterDiag = map(subtract(__, diagDist), absPt);
     
-    return { cur: nextStep, maxDist: max(maxDist, findDist(nextStep)) };
+    return diagDist + afterDiag.x + afterDiag.y / 2;
+}
+
+// CartPair -> Number -> Number -> WalkState
+const walkState = (cur, dist, maxDist) => ({ cur, dist, maxDist });
+
+// CartPair -> CartPair -> CartPair
+const movePos = ({ x: posx, y: posy }, { x: movex, y: movey }) => cartPair(posx + movex, posy + movey);
+
+// WalkState -> CartPair -> WalkState
+const takeStep = ({ cur, maxDist }, step) => {
+    const next = movePos(cur, step);
+    const dist = findDist(next);
+    
+    return walkState(next, dist, max(maxDist, dist));
 };
 
-const walkSteps = reduce(takeStep, { cur: { x: 0, y: 0 }, maxDist: 0 });
+// [String] -> WalkState
+const walkSteps = reduce(takeStep, walkState(cartPair(0,0), 0, 0));
 
-const moveCoordTowardOrigin = c => c - c / Math.abs(c);
-
-const stepTowardOrigin = ({ x, y, steps }) => 
-    ({ x: moveCoordTowardOrigin(x), y: moveCoordTowardOrigin(y), steps: add1(steps) });
-
-const findDist = compose(
-    ({x, y, steps }) => steps + Math.abs(y) / 2 + Math.abs(x),
-    repeatUntil(
-        stepTowardOrigin,
-        ({ x, y }) => x === 0 || y === 0
-    ),
-    merge(__, { steps: 0 })
+// String -> WalkState
+const walkInput = memoizeWith(
+    identity,
+    compose(
+        walkSteps,
+        map(prop(__, stepMap)),
+        split(',')
+    )
 );
 
-const walkInput = compose(
-    walkSteps,
-    map(prop(__, stepMap)),
-    split(',')
-);
-
-const p1 = compose(
-    findDist,
-    prop('cur'),
+// String -> String -> *
+const propFromWalk = targetProp => compose(
+    prop(targetProp),
     walkInput
 );
 
-const p2 = compose(
-    prop('maxDist'),
-    walkInput
-);
+// String -> Number
+const p1 = propFromWalk('dist');
+
+// String -> Number
+const p2 = propFromWalk('maxDist');
 
 module.exports = {
     solution: { 

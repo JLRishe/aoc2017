526f708cfb14f482050344b1773ee83a6f8c0ffe
James Rishe <jlrishe@users.noreply.github.com>
2017-12-18 16:27:56 +0900

    Day 18, part 2

--- 18/index.js
+++ 18/index.js
@@ -1,41 +1,38 @@
 const ramda = require('ramda');
-const { __, compose, map, filter, merge, curry, prop } = ramda;
+const { __, compose, map, filter, merge, curry, prop, isEmpty, any, complement, add, multiply, modulo } = ramda;
 const { probe, applyPattern } = require('../shared');
 const { genTransform, genHead, genFilter } = require('func-generators');
 
 // State is { recovered: Number, lastFreq: Number, pos: Number, regs: {} }
 
+// { Number } -> String -> Number
 const regValue = (regs, reg) => (reg in regs ? regs[reg] : 0);
 
+// { Number } -> String -> Number
 const resolveValue = (regs, value) =>
     !Number.isNaN(parseFloat(value)) ? Number(value) : regValue(regs, value);
 
+const moveNextInstruc = (action) => (state) => merge(action(state), { pos: state.pos + 1 });
+    
+const regUpdateInstruc = op => (x, y) => moveNextInstruc(({ regs, pos }) => ({
+    regs: merge(regs, { [x]: op(regValue(regs, x), resolveValue(regs, y)) })
+}));
+    
 const instrucTypes = {
-    snd: (x) => state => ({ lastFreq: resolveValue(state.regs, x), pos: state.pos + 1 }),
-    set: (x, y) => state => ({ 
-        regs: merge(state.regs, { [x]: resolveValue(state.regs, y) }),
-        pos: state.pos + 1
-    }),
-    add: (x, y) => state => ({
-        regs: merge(state.regs, { [x]: regValue(state.regs, x) + resolveValue(state.regs, y) }),
-        pos: state.pos + 1
-    }),
-    mul: (x, y) => state => ({
-        regs: merge(state.regs, { [x]: regValue(state.regs, x) * resolveValue(state.regs, y) }),
-        pos: state.pos + 1
-    }),
-    mod: (x, y) => state => ({
-        regs: merge(state.regs, { [x]: regValue(state.regs, x) % resolveValue(state.regs, y) }),
-        pos: state.pos + 1
-    }),
-    rcv: (x) => state => 
-        resolveValue(state.regs, x) != 0
-            ? { recovered: state.lastFreq, pos: state.pos + 1 }
-            : { pos: state.pos + 1 },
-    jgz: (x,y) => state =>
-        resolveValue(state.regs, x) > 0
-            ? { pos: state.pos + resolveValue(state.regs, y) }
-            : { pos: state.pos + 1 }
+    snd: (x) => moveNextInstruc(({ regs }) => ({ 
+        lastFreq: resolveValue(regs, x)
+    })),
+    set: regUpdateInstruc((_, yVal) => yVal),
+    add: regUpdateInstruc(add),
+    mul: regUpdateInstruc(multiply),
+    mod: regUpdateInstruc(modulo),
+    rcv: (x) => moveNextInstruc(({ regs, lastFreq }) => 
+        resolveValue(regs, x) != 0 ? { recovered: lastFreq } : { }
+    ),
+    jgz: (x,y) => ({ regs, pos }) =>
+        resolveValue(regs, x) > 0
+            ? { pos: pos + resolveValue(regs, y) }
+            : { pos: pos + 1 }
 };
 
 const parseInstruc = instrucMap => compose(
@@ -45,7 +42,7 @@ const parseInstruc = instrucMap => compose(
 
 const runInstruc = curry((instrucs, state) => {
     const instruc = instrucs[state.pos];
-    
+
     return merge(state, instruc(state));
 });
 
@@ -63,24 +60,47 @@ const p1 = compose(
     map(parseInstruc(instrucTypes))
 );
 
+
 const newInstrucTypes = merge(instrucTypes, {
-    snd: (x) => state => {
-        state.outQueue.push(resolveValue(state.regs, x));
+    snd: (x) => moveNextInstruc(({ regs, outQueue, sndCount }) => {
+        outQueue.push(resolveValue(regs, x));
         
-        return { sndCount: state.sndCount + 1, pos: state.pos + 1 };
-    },
-    rcv: (x) => state => {
-        if (state.inQueue.length === 0) {
+        return { sndCount: sndCount + 1 };
+    }),
+    rcv: (x) => ({ inQueue, regs, pos }) => {
+        if (inQueue.length === 0) {
             return { stopped: true };
         }
-        const received = state.inQueue.shift();
+        const received = inQueue.shift();
         
-        return { regs: merge(state.regs, { [x]: received }), stopped: false, pos: state.pos + 1 };
+        return { regs: merge(regs, { [x]: received }), stopped: false, pos: pos + 1 };
     }
 });
 
+const newExecutor = curry((instrucs, inQueue, outQueue, p) => genTransform(
+    runInstruc(instrucs),
+    { pos: 0, regs: { p }, inQueue, outQueue, stopped: false, sndCount: 0 }
+));
 
-const p2 = () => 0;
+const runUntilStop = (program) => {
+    while (!program.next().value.stopped) {}
+};
+
+const p2 = lines => {
+    const instrucs = map(parseInstruc(newInstrucTypes), lines);
+    
+    const queues = [[], []];
+    const programs = [
+        newExecutor(instrucs, queues[0], queues[1], 0)(),
+        newExecutor(instrucs, queues[1], queues[0], 1)()
+    ];
+    
+    do {
+        programs.forEach(runUntilStop);
+    } while (any(complement(isEmpty), queues));
+    
+    return programs[1].next().value.sndCount;
+};
 
 module.exports = {
     solution: {
